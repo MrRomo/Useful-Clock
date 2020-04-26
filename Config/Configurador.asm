cd4f3747046f87c793ca27b82af2faea1ebbb9ad
Ricardo Romo <jricardoromo21@gmail.com>
2020-04-25 23:30:04 -0500

    Codigo pre Ferney

--- Config/Configurador.asm
+++ Config/Configurador.asm
@@ -1,17 +1,16 @@
 
 _spi_transfer:
 
-;Configurador.c,8 :: 		char spi_transfer(char info){
-;Configurador.c,9 :: 		SSPBUF = READ_TIME_REG;
-	CLRF       SSPBUF+0
+;Configurador.c,8 :: 		unsigned char spi_transfer(unsigned char info){
+;Configurador.c,9 :: 		SSPBUF = info;
+	MOVF       FARG_spi_transfer_info+0, 0
+	MOVWF      SSPBUF+0
 ;Configurador.c,10 :: 		while(!SSPSTAT.BF);
 L_spi_transfer0:
 	BTFSC      SSPSTAT+0, 0
 	GOTO       L_spi_transfer1
 	GOTO       L_spi_transfer0
 L_spi_transfer1:
-;Configurador.c,11 :: 		SSPSTAT.BF = 0;
-	BCF        SSPSTAT+0, 0
 ;Configurador.c,12 :: 		return SSPBUF;
 	MOVF       SSPBUF+0, 0
 	MOVWF      R0+0
@@ -20,124 +19,291 @@ L_end_spi_transfer:
 	RETURN
 ; end of _spi_transfer
 
+_convertValueIN:
+
+;Configurador.c,24 :: 		unsigned char convertValueIN(unsigned char value){
+;Configurador.c,25 :: 		unsigned char convertedVal = value - 6 * (value >> 4) ;
+	MOVF       FARG_convertValueIN_value+0, 0
+	MOVWF      R0+0
+	RRF        R0+0, 1
+	BCF        R0+0, 7
+	RRF        R0+0, 1
+	BCF        R0+0, 7
+	RRF        R0+0, 1
+	BCF        R0+0, 7
+	RRF        R0+0, 1
+	BCF        R0+0, 7
+	MOVLW      6
+	MOVWF      R4+0
+	CALL       _Mul_8X8_U+0
+	MOVF       R0+0, 0
+	SUBWF      FARG_convertValueIN_value+0, 0
+	MOVWF      R0+0
+	MOVF       R0+1, 0
+	BTFSS      STATUS+0, 0
+	ADDLW      1
+	CLRF       R0+1
+	SUBWF      R0+1, 1
+;Configurador.c,26 :: 		return convertedVal ;
+;Configurador.c,27 :: 		}
+L_end_convertValueIN:
+	RETURN
+; end of _convertValueIN
+
+_convertValueOUT:
+
+;Configurador.c,28 :: 		unsigned char convertValueOUT(unsigned char value){
+;Configurador.c,29 :: 		unsigned char convertedVal = value + 6 * (value / 10);
+	MOVLW      10
+	MOVWF      R4+0
+	MOVF       FARG_convertValueOUT_value+0, 0
+	MOVWF      R0+0
+	CALL       _Div_8X8_U+0
+	MOVLW      6
+	MOVWF      R4+0
+	CALL       _Mul_8X8_U+0
+	MOVF       FARG_convertValueOUT_value+0, 0
+	ADDWF      R0+0, 1
+	BTFSC      STATUS+0, 0
+	INCF       R0+1, 1
+;Configurador.c,30 :: 		return convertedVal ;
+;Configurador.c,31 :: 		}
+L_end_convertValueOUT:
+	RETURN
+; end of _convertValueOUT
+
+_print_date:
+
+;Configurador.c,33 :: 		void print_date(timeParameters_t * dataTime){
+;Configurador.c,34 :: 		UART1_Write_Text(dataTime->ss);
+	MOVF       FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,35 :: 		UART1_Write_Text(dataTime->mm);
+	INCF       FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,36 :: 		UART1_Write_Text(dataTime->hh);
+	MOVLW      2
+	ADDWF      FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,37 :: 		UART1_Write_Text(dataTime->dy);
+	MOVLW      3
+	ADDWF      FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,38 :: 		UART1_Write_Text(dataTime->d);
+	MOVLW      4
+	ADDWF      FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,39 :: 		UART1_Write_Text(dataTime->m);
+	MOVLW      5
+	ADDWF      FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,40 :: 		UART1_Write_Text(dataTime->y);
+	MOVLW      6
+	ADDWF      FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,41 :: 		UART1_Write_Text(dataTime->d);
+	MOVLW      4
+	ADDWF      FARG_print_date_dataTime+0, 0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,42 :: 		}
+L_end_print_date:
+	RETURN
+; end of _print_date
+
 _get_time:
 
-;Configurador.c,15 :: 		void get_time(){
-;Configurador.c,17 :: 		char i = 0;
-	CLRF       get_time_i_L0+0
-;Configurador.c,18 :: 		CS = 0;
+;Configurador.c,43 :: 		void get_time(){
+;Configurador.c,45 :: 		char i = 0;
+;Configurador.c,47 :: 		CS = 0;
 	BCF        PORTD+0, 0
-;Configurador.c,19 :: 		UART1_Write_Text("LEYENDO TIEMPO");
+;Configurador.c,48 :: 		Delay_ms(100);
+	MOVLW      130
+	MOVWF      R12+0
+	MOVLW      221
+	MOVWF      R13+0
+L_get_time2:
+	DECFSZ     R13+0, 1
+	GOTO       L_get_time2
+	DECFSZ     R12+0, 1
+	GOTO       L_get_time2
+	NOP
+	NOP
+;Configurador.c,49 :: 		UART1_Write_Text("LEYENDO TIEMPO");
 	MOVLW      ?lstr1_Configurador+0
 	MOVWF      FARG_UART1_Write_Text_uart_text+0
 	CALL       _UART1_Write_Text+0
-;Configurador.c,20 :: 		for (i = 0; i < 8; i++)
-	CLRF       get_time_i_L0+0
-L_get_time2:
-	MOVLW      8
-	SUBWF      get_time_i_L0+0, 0
-	BTFSC      STATUS+0, 0
-	GOTO       L_get_time3
-;Configurador.c,22 :: 		IntToStr(spi_transfer(READ_TIME_REG),text);
+;Configurador.c,50 :: 		spi_transfer(READ_TIME_REG) ;
+	CLRF       FARG_spi_transfer_info+0
+	CALL       _spi_transfer+0
+;Configurador.c,51 :: 		timeVals->ss = convertValueIN(spi_transfer(0x00));
+	MOVF       get_time_timeVals_L0+0, 0
+	MOVWF      FLOC__get_time+0
 	CLRF       FARG_spi_transfer_info+0
 	CALL       _spi_transfer+0
 	MOVF       R0+0, 0
-	MOVWF      FARG_IntToStr_input+0
-	CLRF       FARG_IntToStr_input+1
-	MOVLW      get_time_text_L0+0
-	MOVWF      FARG_IntToStr_output+0
-	CALL       _IntToStr+0
-;Configurador.c,23 :: 		UART1_Write_Text(text);
-	MOVLW      get_time_text_L0+0
-	MOVWF      FARG_UART1_Write_Text_uart_text+0
-	CALL       _UART1_Write_Text+0
-;Configurador.c,20 :: 		for (i = 0; i < 8; i++)
-	INCF       get_time_i_L0+0, 1
-;Configurador.c,24 :: 		}
-	GOTO       L_get_time2
-L_get_time3:
-;Configurador.c,25 :: 		CS = 1;
+	MOVWF      FARG_convertValueIN_value+0
+	CALL       _convertValueIN+0
+	MOVF       FLOC__get_time+0, 0
+	MOVWF      FSR
+	MOVF       R0+0, 0
+	MOVWF      INDF+0
+;Configurador.c,52 :: 		timeVals->mm = convertValueIN(spi_transfer(0x01));
+	INCF       get_time_timeVals_L0+0, 0
+	MOVWF      FLOC__get_time+0
+	MOVLW      1
+	MOVWF      FARG_spi_transfer_info+0
+	CALL       _spi_transfer+0
+	MOVF       R0+0, 0
+	MOVWF      FARG_convertValueIN_value+0
+	CALL       _convertValueIN+0
+	MOVF       FLOC__get_time+0, 0
+	MOVWF      FSR
+	MOVF       R0+0, 0
+	MOVWF      INDF+0
+;Configurador.c,58 :: 		CS = 1;
 	BSF        PORTD+0, 0
-;Configurador.c,26 :: 		}
+;Configurador.c,60 :: 		}
 L_end_get_time:
 	RETURN
 ; end of _get_time
 
+_setTime:
+
+;Configurador.c,61 :: 		void setTime(){
+;Configurador.c,62 :: 		UART1_Write_Text("Seteando el tiempo");
+	MOVLW      ?lstr2_Configurador+0
+	MOVWF      FARG_UART1_Write_Text_uart_text+0
+	CALL       _UART1_Write_Text+0
+;Configurador.c,63 :: 		CS = 0;
+	BCF        PORTD+0, 0
+;Configurador.c,64 :: 		Delay_ms(100);
+	MOVLW      130
+	MOVWF      R12+0
+	MOVLW      221
+	MOVWF      R13+0
+L_setTime3:
+	DECFSZ     R13+0, 1
+	GOTO       L_setTime3
+	DECFSZ     R12+0, 1
+	GOTO       L_setTime3
+	NOP
+	NOP
+;Configurador.c,65 :: 		spi_transfer(0x81);
+	MOVLW      129
+	MOVWF      FARG_spi_transfer_info+0
+	CALL       _spi_transfer+0
+;Configurador.c,66 :: 		spi_transfer(0x19);
+	MOVLW      25
+	MOVWF      FARG_spi_transfer_info+0
+	CALL       _spi_transfer+0
+;Configurador.c,67 :: 		CS = 1;
+	BSF        PORTD+0, 0
+;Configurador.c,68 :: 		Delay_ms(10) ;
+	MOVLW      13
+	MOVWF      R12+0
+	MOVLW      251
+	MOVWF      R13+0
+L_setTime4:
+	DECFSZ     R13+0, 1
+	GOTO       L_setTime4
+	DECFSZ     R12+0, 1
+	GOTO       L_setTime4
+	NOP
+	NOP
+;Configurador.c,69 :: 		}
+L_end_setTime:
+	RETURN
+; end of _setTime
+
+_RTC_init:
+
+;Configurador.c,72 :: 		void RTC_init(char freq){
+;Configurador.c,74 :: 		CS = 0;
+	BCF        PORTD+0, 0
+;Configurador.c,75 :: 		spi_transfer(READ_CONTROL_REG);
+	MOVLW      14
+	MOVWF      FARG_spi_transfer_info+0
+	CALL       _spi_transfer+0
+;Configurador.c,76 :: 		config = spi_transfer(0x00);
+	CLRF       FARG_spi_transfer_info+0
+	CALL       _spi_transfer+0
+;Configurador.c,77 :: 		CS = 1;
+	BSF        PORTD+0, 0
+;Configurador.c,78 :: 		Delay_ms(100);
+	MOVLW      130
+	MOVWF      R12+0
+	MOVLW      221
+	MOVWF      R13+0
+L_RTC_init5:
+	DECFSZ     R13+0, 1
+	GOTO       L_RTC_init5
+	DECFSZ     R12+0, 1
+	GOTO       L_RTC_init5
+	NOP
+	NOP
+;Configurador.c,79 :: 		}
+L_end_RTC_init:
+	RETURN
+; end of _RTC_init
+
 _main:
 
-;Configurador.c,28 :: 		void main() {
-;Configurador.c,30 :: 		UART1_Init(9600);
+;Configurador.c,81 :: 		void main() {
+;Configurador.c,83 :: 		UART1_Init(9600);
 	MOVLW      25
 	MOVWF      SPBRG+0
 	BSF        TXSTA+0, 2
 	CALL       _UART1_Init+0
-;Configurador.c,31 :: 		UART1_Write_Text("inicio de la prueba");
-	MOVLW      ?lstr2_Configurador+0
-	MOVWF      FARG_UART1_Write_Text_uart_text+0
-	CALL       _UART1_Write_Text+0
-;Configurador.c,32 :: 		TRISC.F3 = 0; //CLK SPI
-	BCF        TRISC+0, 3
-;Configurador.c,33 :: 		TRISC.F5 = 0; //MOSI - SDO
-	BCF        TRISC+0, 5
-;Configurador.c,34 :: 		TRISC.F4 = 1; //MISO - SDI
-	BSF        TRISC+0, 4
-;Configurador.c,35 :: 		TRISD.F0 = 0; //CS
-	BCF        TRISD+0, 0
-;Configurador.c,36 :: 		SSPSTAT = 0X40;
-	MOVLW      64
-	MOVWF      SSPSTAT+0
-;Configurador.c,37 :: 		SSPCON =  0X20;
-	MOVLW      32
-	MOVWF      SSPCON+0
-;Configurador.c,38 :: 		CS = 1;
-	BSF        PORTD+0, 0
-;Configurador.c,39 :: 		Delay_ms(1000);
-	MOVLW      6
-	MOVWF      R11+0
-	MOVLW      19
-	MOVWF      R12+0
-	MOVLW      173
-	MOVWF      R13+0
-L_main5:
-	DECFSZ     R13+0, 1
-	GOTO       L_main5
-	DECFSZ     R12+0, 1
-	GOTO       L_main5
-	DECFSZ     R11+0, 1
-	GOTO       L_main5
-	NOP
-	NOP
-;Configurador.c,40 :: 		CS = 0;
-	BCF        PORTD+0, 0
-;Configurador.c,41 :: 		UART1_Write_Text("Configurando: ");
+;Configurador.c,84 :: 		UART1_Write_Text("inicio de la prueba");
 	MOVLW      ?lstr3_Configurador+0
 	MOVWF      FARG_UART1_Write_Text_uart_text+0
 	CALL       _UART1_Write_Text+0
-;Configurador.c,42 :: 		spi_transfer(READ_CONTROL_REG);
-	MOVLW      14
-	MOVWF      FARG_spi_transfer_info+0
-	CALL       _spi_transfer+0
-;Configurador.c,43 :: 		IntToStr(spi_transfer(0x00),text);
-	CLRF       FARG_spi_transfer_info+0
-	CALL       _spi_transfer+0
-	MOVF       R0+0, 0
-	MOVWF      FARG_IntToStr_input+0
-	CLRF       FARG_IntToStr_input+1
-	MOVLW      main_text_L0+0
-	MOVWF      FARG_IntToStr_output+0
-	CALL       _IntToStr+0
-;Configurador.c,44 :: 		UART1_Write_Text(text);
-	MOVLW      main_text_L0+0
-	MOVWF      FARG_UART1_Write_Text_uart_text+0
-	CALL       _UART1_Write_Text+0
-;Configurador.c,45 :: 		CS = 1;
+;Configurador.c,85 :: 		TRISC.F3 = 0; //CLK SPI
+	BCF        TRISC+0, 3
+;Configurador.c,86 :: 		TRISC.F5 = 0; //MOSI - SDO
+	BCF        TRISC+0, 5
+;Configurador.c,87 :: 		TRISC.F4 = 1; //MISO - SDI
+	BSF        TRISC+0, 4
+;Configurador.c,88 :: 		TRISD.F0 = 0; //CS
+	BCF        TRISD+0, 0
+;Configurador.c,89 :: 		SSPSTAT = 0X40;
+	MOVLW      64
+	MOVWF      SSPSTAT+0
+;Configurador.c,90 :: 		SSPCON =  0X20;
+	MOVLW      32
+	MOVWF      SSPCON+0
+;Configurador.c,91 :: 		CS = 1;
 	BSF        PORTD+0, 0
-;Configurador.c,46 :: 		Delay_ms(1000);
-	MOVLW      6
+;Configurador.c,92 :: 		Delay_ms(2000);
+	MOVLW      11
 	MOVWF      R11+0
-	MOVLW      19
+	MOVLW      38
 	MOVWF      R12+0
-	MOVLW      173
+	MOVLW      93
 	MOVWF      R13+0
 L_main6:
 	DECFSZ     R13+0, 1
@@ -148,29 +314,17 @@ L_main6:
 	GOTO       L_main6
 	NOP
 	NOP
-;Configurador.c,47 :: 		while(1){
+;Configurador.c,93 :: 		RTC_init(1);
+	MOVLW      1
+	MOVWF      FARG_RTC_init_freq+0
+	CALL       _RTC_init+0
+;Configurador.c,94 :: 		setTime();
+	CALL       _setTime+0
+;Configurador.c,95 :: 		while(1){
 L_main7:
-;Configurador.c,48 :: 		get_time();
-	CALL       _get_time+0
-;Configurador.c,49 :: 		Delay_ms(1500);
-	MOVLW      8
-	MOVWF      R11+0
-	MOVLW      157
-	MOVWF      R12+0
-	MOVLW      5
-	MOVWF      R13+0
-L_main9:
-	DECFSZ     R13+0, 1
-	GOTO       L_main9
-	DECFSZ     R12+0, 1
-	GOTO       L_main9
-	DECFSZ     R11+0, 1
-	GOTO       L_main9
-	NOP
-	NOP
-;Configurador.c,50 :: 		}
+;Configurador.c,97 :: 		}
 	GOTO       L_main7
-;Configurador.c,51 :: 		}
+;Configurador.c,98 :: 		}
 L_end_main:
 	GOTO       $+0
 ; end of _main
--- Config/Configurador.c
+++ Config/Configurador.c
@@ -5,24 +5,77 @@
 #define WRITE_TIME_REG 0x80
 #define READ_TIME_REG 0x00
 
-char spi_transfer(char info){
-   SSPBUF = READ_TIME_REG;
+unsigned char spi_transfer(unsigned char info){
+   SSPBUF = info;
    while(!SSPSTAT.BF);
    SSPSTAT.BF = 0;
    return SSPBUF;
 }
+typedef struct timeParameters{
+  unsigned char ss ;
+  unsigned char mm ;
+  unsigned char hh ;
+  unsigned char dy ;
+  unsigned char d ;
+  unsigned char m ;
+  unsigned char y ;
+}timeParameters_t ;
 
+unsigned char convertValueIN(unsigned char value){
+  unsigned char convertedVal = value - 6 * (value >> 4) ;
+  return convertedVal ;
+}
+unsigned char convertValueOUT(unsigned char value){
+  unsigned char convertedVal = value + 6 * (value / 10);
+  return convertedVal ;
+}
+
+void print_date(timeParameters_t * dataTime){
+   UART1_Write_Text(dataTime->ss);
+   UART1_Write_Text(dataTime->mm);
+   UART1_Write_Text(dataTime->hh);
+   UART1_Write_Text(dataTime->dy);
+   UART1_Write_Text(dataTime->d);
+   UART1_Write_Text(dataTime->m);
+   UART1_Write_Text(dataTime->y);
+   UART1_Write_Text(dataTime->d);
+}
 void get_time(){
-   char text[10];
+   timeParameters_t * timeVals;
    char i = 0;
-   CS = 0;               
+   char text [10];
+   CS = 0;
+   Delay_ms(100);
    UART1_Write_Text("LEYENDO TIEMPO");
-   for (i = 0; i < 8; i++)
-   {
-   IntToStr(spi_transfer(READ_TIME_REG),text);
-   UART1_Write_Text(text);
-   }
+   spi_transfer(READ_TIME_REG) ;
+   timeVals->ss = convertValueIN(spi_transfer(0x00));
+   timeVals->mm = convertValueIN(spi_transfer(0x01));
+   // timeVals->hh = convertValueIN(spi_transfer(0x00));
+   // timeVals->dy = convertValueIN(spi_transfer(0x00));
+   // timeVals->d = convertValueIN(spi_transfer(0x00));
+   // timeVals->m = convertValueIN(spi_transfer(0x00));
+   // timeVals->y = convertValueIN(spi_transfer(0x00));
    CS = 1;
+   // print_date(timeVals);
+}
+void setTime(){
+   UART1_Write_Text("Seteando el tiempo");
+   CS = 0;
+   Delay_ms(100);
+   spi_transfer(0x81);
+   spi_transfer(0x19);
+   CS = 1;
+   Delay_ms(10) ;
+}
+
+
+void RTC_init(char freq){
+   unsigned char config;
+   CS = 0;
+   spi_transfer(READ_CONTROL_REG);
+   config = spi_transfer(0x00);
+   CS = 1;
+   Delay_ms(100);
 }
 
 void main() {
@@ -36,16 +89,10 @@ void main() {
    SSPSTAT = 0X40;
    SSPCON =  0X20;
    CS = 1;
-   Delay_ms(1000);
-   CS = 0;
-   UART1_Write_Text("Configurando: ");
-   spi_transfer(READ_CONTROL_REG);
-   IntToStr(spi_transfer(0x00),text);
-   UART1_Write_Text(text);
-   CS = 1;
-   Delay_ms(1000);
+   Delay_ms(2000);
+   RTC_init(1);
+   setTime();
    while(1){
-      get_time();
-      Delay_ms(1500);
+
    }
 }
